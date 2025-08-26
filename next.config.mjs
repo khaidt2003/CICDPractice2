d5c3b8efe9e4acadc3346597f533f9c43799db6e
Khai Nguyen <cuong.0906140073@gmail.com>
2025-08-26 21:14:48 +0700

    chor: add yaml file

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,9 @@
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: {
-    loader: "akamai",
-    path: "",
-  },
-  assetPrefix: "./",
+  output: 'export',
+  images: { unoptimized: true }, // cần khi export
+  // Nếu deploy vào project site (không phải <user>.github.io):
+  // basePath: '/<ten-repo>',
+  // assetPrefix: '/<ten-repo>/',
 };
-
 export default nextConfig;
